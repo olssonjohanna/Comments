8b013e6d7fa2108887de7a492887042bd3611c08
olssonjohanna <32932208+olssonjohanna@users.noreply.github.com>
2017-10-30 13:16:33 +0100

    comments

--- Client/SocketHandler.py
+++ Client/SocketHandler.py
@@ -1,15 +1,16 @@
 import socket
 import _thread
 
-#
-
+#Vår konstruktor som skapar en client socket
 class SocketHandler:
     def __init__(self):
         self.clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+#Skapar en self av inkommande variable: guihandler
     def setGuiHandler(self,guiHandler_):
         self.guiHandler = guiHandler_
 
+#Denna funktion får IP och port. Försöker att connecta och vid sucess: sätter igång en funktion startRecieverThread. Om inte: return "no connection"
     def connect(self,ip, port):
         try:
             self.clientSocket.connect((ip,int(port)))
@@ -17,15 +18,18 @@ class SocketHandler:
         except:
             return "no connection"
 
+#En funktion som försöker att skicka meddelande till servern
     def sendMsg(self,text):
         try:
             self.clientSocket.send(str.encode(text))
         except:
             pass
 
+#En funktion som skapar en tråd, som ska göra funktionen_ startRecieving
     def startReceiverThread(self):
         _thread.start_new_thread(self.startReceiving,())
 
+#Funktionen tar emot oändligt med messages, vid mottagande så startar den funktionen: showMessage med det recv meddelandet vid except: visar den deconnected.
     def startReceiving(self):
         while True:
             try:
