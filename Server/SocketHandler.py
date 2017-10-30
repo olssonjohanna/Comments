dc0dcb35e825042de0936f78b53eeca707bdd49c
Axelhagnell <32957679+Axelhagnell@users.noreply.github.com>
2017-10-30 14:29:16 +0100

    okej

--- Server/SocketHandler.py
+++ Server/SocketHandler.py
@@ -60,64 +60,68 @@ class SocketHandler:
     def startReceiverThread(self, clientSocket, clientAddr):
         _thread.start_new_thread(self.startReceiving,(clientSocket,clientAddr,))
 
+
     def startReceiving(self,clientSocket, clientAddr):
-        resultOfLogin = self.listenToUnknownClinet(clientSocket,clientAddr)
+        resultOfLogin = self.listenToUnknownClinet(clientSocket,clientAddr)#resultatet av inloggnigen
 
         if resultOfLogin !=False:
-            username = resultOfLogin
+            username = resultOfLogin#usernamet är lika med det returnade värdet ifrån listenToUknownClient
+            #när inloggningen har accepterats tas socketen bort ifrån unknown och flyttas till known sockets
             self.list_of_unknown_clientSockets.remove(clientSocket)
             self.list_of_unknown_clientAddr.remove(clientAddr)
 
             self.list_of_known_clientSockets.append(clientSocket)
             self.list_of_known_clientAddr.append(clientAddr)
 
-            self.listenToknownClinet(clientSocket,clientAddr,username)
+            self.listenToknownClinet(clientSocket,clientAddr,username)#skickar våran clientsocket, address och username till
+            #listenTOknownClient som börjar vänta in meddelandet ifrån socketen.
 
-#
+    #lyssnar på den okända klienten
     def listenToUnknownClinet(self,clientSocket, clientAddr):
         while True:
             try:
-                msg = clientSocket.recv(1024).decode()
+                msg = clientSocket.recv(1024).decode()#väntar på att ta emot ett meddelande ifrån ökända klienten
             except:
-                self.list_of_unknown_clientSockets.remove(clientSocket)
+                self.list_of_unknown_clientSockets.remove(clientSocket)#om inget kommer tas klienten bort ifrån listan
                 self.list_of_unknown_clientAddr.remove(clientAddr)
                 return False
 
-            args = msg.split(' ')
-            if len(args) == 3 and args[0] == "login":
-                username = args[1]
-                password = args[2]
-                if self.users.doesThisUserExistAndNotActive(username,password):
-                    clientSocket.send(str.encode("ok"))
-                    self.sendAndShowMsg(username + " is connected")
+            args = msg.split(' ')#splittar det mottagna meddelandet till en lista
+            if len(args) == 3 and args[0] == "login":#om den innehåller 3 olika indexar och första indexet är "login"
+                username = args[1]#username är lika med den andra indexen
+                password = args[2]#password är lika med den tredje
+                if self.users.doesThisUserExistAndNotActive(username,password):#om användaren finns och inte är aktiv i chatten
+                    clientSocket.send(str.encode("ok"))#skickar till ClientSocketHandler att det gick att logga in
+                    self.sendAndShowMsg(username + " is connected")#visar ett meddelande i chattrutan att "username" har connectat
                     return username
                 else:
-                    clientSocket.send(str.encode("not ok"))
+                    clientSocket.send(str.encode("not ok"))#skickar att det inte gick att logga in
 
-            if len(args) >= 5 and args[0] == "register":
+            if len(args) >= 5 and args[0] == "register":#om meddelandet är mindre eller lika med 5 och första indexet är "register"
                 username = args[1]
                 password = args[2]
                 email = args[3]
                 name = ""
-                for rest in args[4:]:
+                for rest in args[4:]:#lägger in alla namn i samma rad
                     name += rest + " "
-                if username != "" and password != "" and email != "" and name != "":
-                    resultOfAdding = self.users.add_user(username,password,email,name)
+                if username != "" and password != "" and email != "" and name != "":#om det inte innehåller några tomma rader
+                    resultOfAdding = self.users.add_user(username,password,email,name)#kollar om det inte finns någon med samma username
                     if resultOfAdding == True:
-                        clientSocket.send(str.encode("fine"))
+                        clientSocket.send(str.encode("fine"))#skickar att det gick att registreraa
                     else:
-                        clientSocket.send(str.encode("not fine"))
+                        clientSocket.send(str.encode("not fine"))#skickar att det inte gick att registrera
                 else:
-                    clientSocket.send(str.encode("not fine"))
+                    clientSocket.send(str.encode("not fine"))#skickar att det inte gick att registrera
 
-    def listenToknownClinet(self,clientSocket, clientAddr,username):
+    def listenToknownClinet(self,clientSocket, clientAddr,username):#recieve och send funktion till kända klienter
         while True:
             try:
                 msg = clientSocket.recv(1024).decode()
-                self.sendAndShowMsg(username + ": " + msg)
+                self.sendAndShowMsg(username + ": " + msg)#skickar meddelande med username
+            #om någon disconnectar skrivs det i chattrutan
             except:
                 self.list_of_known_clientSockets.remove(clientSocket)
                 self.list_of_known_clientAddr.remove(clientAddr)
                 self.sendAndShowMsg(username+" disconnected")
-                self.users.inactiveUser(username)
+                self.users.inactiveUser(username)#inactiverar användaren
                 return
